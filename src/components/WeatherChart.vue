59f8620a6afde71cb1a680f4668ac17737e95ef7
“SheikhFaizan141” <faizanfarooq141@gmail.com>
2023-09-30 16:59:40 +0530

    time label not yet added need to figure out way to do so

--- src/components/WeatherChart.vue
+++ src/components/WeatherChart.vue
@@ -11,7 +11,8 @@ import {
     LineElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
+    LogarithmicScale
 } from 'chart.js';
 import { formatUnit } from '../global/utils';
 
@@ -74,12 +75,13 @@ function getData() {
         return formatUnit(item['temp'], props.unit);
     })
 
+    console.log(props.hourlyForecast[0]);
     return res;
 }
 
 function getLabels() {
     return props.hourlyForecast.map(item => {
-        console.log(item.weather);
+        // console.log(item.weather);
         return item.weather[0].description;
     })
 }
@@ -88,42 +90,94 @@ const data = getData();
 const labels = getLabels();
 
 const chartData = ref({
-    labels: labels,
+    labels: labels.slice(0, 8),
     datasets: [{
-        label: 'faf',
+        label: 'temp',
         data: data,
+        // clip: 1,
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
-        tension: 0.2
-    }]
+        tension: 0.4
+    }],
+
 });
 
 const chartOptions = ref({
     responsive: true,
-    pointRadius: 0,
+    // pointRadius: 0,
     scales: {
-            y: {
-                grid: {
-                    display: false
-                },
-                // displa
-                suggestedMin: Math.min(...data)  - 5,
-                suggestedMax: Math.max(...data) + 5
+        y: {
+            grid: {
+                display: false,
             },
-            x: {
-                grid: {
-                    display: false
+            // display: false,
+            // suggestedMin: Math.min(...data)  - 5,
+            // suggestedMax: Math.max(...data) + 5,
+            // suggestedMin: Math.min(...data.slice(0, 8)) - 5,
+            // suggestedMax: Math.max(...data.slice(0, 8)) + 5,
+            // min: Math.min(...data.slice(0, 8))  - 5,
+            // max: Math.max(...data.slice(0, 8)) + 5,
+            border: {
+                display: false
+            },
+            // weight: 10,
+            // title: {
+            //     text: 'temperature',
+            //     align: 'center',
+            //     color: 'gray',
+            //     display: true,
+            //     fontSize: '20px',
+            // },
+            ticks: {
+                // Include a dollar sign in the ticks
+                callback: function (value, index, ticks) {
+                    return value + '°';
                 },
-                display: true
+                padding: 5,
+                color: 'rgb(235 110 75)',
+                stepSize: 5,
             }
+
+        },
+        x: {
+            grid: {
+                // display: false,
+                color: 'transparent',
+                borderColor: 'red',
+                tickColor: 'grey'
+            },
+            border: {
+                color: 'black'
+            },
+            display: true,
+            // https://www.chartjs.org/docs/latest/axes/labelling.html
+            // title: {
+            //     text: 'discription',
+            //     align: 'center',
+            //     color: 'orange',
+            //     display: true
+            // },
+            ticks: {
+                autoSkip: true,
+                maxTicksLimit: 8,
+            }
+        },
+    },
+    plugins: {
+        legend: {
+            display: false
         }
 
+    },
+    layout: {
+        padding: 5
+    }
 })
 
 </script>
 
 <template>
-    <h1>Hourly Weather</h1>
+    <h2>Hourly forecast</h2>
     <Line
         v-if="hourlyForecast"
         id="my-chart-id"
